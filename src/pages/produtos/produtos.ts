86f83b1878ae494701a5c6e630c6b7b47e4781ee
Edgleidson <wake-up88@hotmail.com>
2022-01-31 16:51:57 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../service/domain/produto.service';
 
@@ -24,8 +25,21 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
       error => {});
   }
 
+  //Carregando as URLs das imagens.
+  loadImageUrls(){
+    for(var i=0; i<this.items.length; i++){
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
+        },
+        error => {});
+        
+    }
+  }
 }
